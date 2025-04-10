60ce3cc726adcac774b39a1ed0ebcaff271ed48f
Richard Yin <r.yin@mail.utoronto.ca>
2025-04-09 21:41:04 -0400

    Removed sketchy helper function

--- A3/frontend/src/utils/utils.jsx
+++ A3/frontend/src/utils/utils.jsx
@@ -1,5 +1,3 @@
-import { useLocation, useHistory } from "react-router-dom";
-
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 export async function fetchServer(path, details, errors) {
     const response = await fetch(`${BACKEND_URL}/${path}`, details);
@@ -18,25 +16,4 @@ export function hasPerms(levelHas, levelNeeded) {
     const needed = PERMISSION_LEVELS.findIndex(x => x == levelNeeded);
     return has >= needed;
 }
-
-export function useQueryParams() {
-    const location = useLocation();
-    const history = useHistory();
-  
-    const addQueryParam = (key, value) => {
-        const searchParams = new URLSearchParams(location.search);
-        searchParams.set(key, value);
-        history.push({
-            search: searchParams.toString(),
-        });
-    };
-    const removeQueryParam = (key) => {
-        const searchParams = new URLSearchParams(location.search);
-        searchParams.delete(key);
-        history.push({
-            search: searchParams.toString(),
-        });
-    };
-    return { addQueryParam, removeQueryParam };
-};
   
