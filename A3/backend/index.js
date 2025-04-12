b8ed246db36b9d9315daec05f47f70f97dcce941
kailimprice <kailimprice@gmail.com>
2025-04-11 20:05:11 -0400

    Add cors credentials

--- A3/backend/index.js
+++ A3/backend/index.js
@@ -29,7 +29,7 @@ require('dotenv').config();
 
 // Set up cors to allow requests from your React frontend
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
-app.use(cors({origin: FRONTEND_URL}));
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
 
 
 const {PrismaClient} = require('@prisma/client');
