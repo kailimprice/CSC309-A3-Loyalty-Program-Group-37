953a5302dbb3647556b73792c4fdf182a964c001
kailimprice <kailimprice@gmail.com>
2025-04-11 23:23:45 -0400

    Test

--- A3/backend/index.js
+++ A3/backend/index.js
@@ -1772,6 +1772,8 @@ app.delete('/promotions/:promotionId', permLevel('manager'), async (req, res) =>
 
 
 app.use((req, res, next) => {
+    console.log(req)
+    console.log(res)
     return res.status(405).json({"error": `Unsupported method ${req.method}`});
 })
 
